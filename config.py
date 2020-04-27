7e09ac2baa939900e814415e0ac1be9c5b783568
goldkin <itisdark@gmail.com>
2020-04-27 03:14:36 -0700

    Add some nuance to the configurator
    Handles corner cases present in Berserker's multiworld YAML configuration

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ from shutil import copy
 from urllib.parse import quote
 from yaml import load, dump, SafeLoader
 
+
 class PlayerConfig:
     def create_player_from_template(self, templatefile, outfile):
         ''' Load template for a new player.
@@ -15,6 +16,11 @@ class PlayerConfig:
         while '/' in path:
             next, path = path.split('/', 1)
             elem = elem[next]
+        # Due to unfortunate YAML parsing, we need to check if a key can be an int
+        try:
+            path = int(path)
+        except ValueError:
+            pass
         return elem[path]
 
     def update_player_setting(self, playerfile, path, value):
@@ -24,10 +30,22 @@ class PlayerConfig:
         while '/' in path:
             next, path = path.split('/', 1)
             elem = elem[next]
+        # Due to unfortunate YAML parsing, we need to check if a key can be an int
+        try:
+            path = int(path)
+        except ValueError:
+            pass
         elem[path] = self.validate_input(value)
         open(playerfile, 'w').write(dump(player_dict))
         return 'Updated successfully!'
 
     def validate_input(self, value):
-        ''' Generic numeric validator for YAML input.'''
+        ''' Generic boolean and numeric validator for YAML input.'''
+        value = value.lower()
+        if value == 'false':
+            return False
+        elif value == 'true':
+            return True
+        if value == 'none':
+            return None
         return int(value)
